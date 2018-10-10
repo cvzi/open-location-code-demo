d4a5b8f60ff0dad2af7121350a5e6001f78000bb
cvzi <cuzi@openmail.cc>
2018-10-10 17:27:42 +0200

    Initial map position

--- script.js
+++ script.js
@@ -8,9 +8,8 @@ var map;
 var iv;
 var layers = [];
 
-
 function main() {
-  map = L.map('mapid').setView([49.41, -8.71], 11);
+  map = L.map('mapid').setView([49.41, 8.71], 11);
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
       attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
       maxZoom: 18,
